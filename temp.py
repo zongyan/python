8a4f4b124a2e28584b08b43abc573f6f7473d2bf
Yan Zong <yan@pkcos.com>
2021-01-08 10:30:10 +0800

    Data type for python3

--- temp.py
+++ temp.py
@@ -61,6 +61,7 @@ for i in sys.argv:
     print (i)
 print(sys.argv)
 print("sys.argv")
+
 print("=================Data Type==========================")
 m=1
 print(m, end="")
@@ -76,3 +77,88 @@ a,b,c=1,2,"zongyan"
 print(a,b,c)
 a, b, c, d = 20, 5.5, True, 4+3j
 print(type(a), type(b), type(c), type(d))
+a = 111
+isinstance(a, float) # no results, need to use the print function 
+print(isinstance(a, float))
+print(isinstance(a, int))
+a = 2
+print(a)
+# del a # delete variable a 
+print(a)
+a=5
+b=4
+print(a+b, a - b, a/b, a//b, a%b, a**b)
+llist = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]
+tinylist = [123, 'runoob']
+print(llist)
+print(llist[0])
+print(llist[1:3])
+print(llist[2:])
+print (llist + tinylist)
+a=[0, 1, 2, 3, 4, 5, 6]
+print(a[0])
+print(a[2:5])
+a[2:5] = [13, 14, 15]
+print(a)
+##define a function for reversing the word
+def reverseWords(input):
+    inputWords = input.split(" ")
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],  
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    inputWords=inputWords[-1::-1]    
+    
+    # 重新组合字符串 https://www.runoob.com/python/att-string-join.html
+    output = ' '.join(inputWords)
+    return output
+
+if __name__ == "__main__":
+    inputs = "Yan like YuXin"
+    rw = reverseWords(inputs)
+    print(rw)
+
+ttuple = ( 'abcd', 786 , 2.23, 'runoob', 70.2  )
+tinytuple = (123, 'runoob')
+print(ttuple)
+print(ttuple[0])
+print(ttuple[1:3])
+print(ttuple[2:])
+print(tinytuple * 2)    
+print(tinytuple + ttuple)        
+tup1 = () 
+print(tup1)    
+tup2 = (12,)     
+print(tup2)        
+sites = {"Google", "Taobao", "Runoob", "Facebook", "Zhihu", "Baidu"}
+print(sites) # 怎样感觉输出的时候，没有一个特定的顺序输出的呢？这个没有看明白
+sites = {"Google", "Taobao", "Runoob", "Facebook", "Zhihu", "Baidu", "Zhihu"}
+print(sites)    
+a=set("abracadabra"); b = set("alacazam")
+print(a-b)  # a 和 b 的差集
+print(a | b) # a 和 b 的并集
+print(a & b)  # a 和 b 的交集
+print(a ^ b) # a 和 b 中不同时存在的元素
+
+ddict={}
+ddict["one"] = "1 - 菜鸟教程"
+print(ddict["one"])
+print(ddict)
+ddict[2] = "2 - 菜鸟工具"
+print(ddict[2])
+print(ddict)
+# 从上面这两个例子，就是可以清楚的明白，key的概念，还有value的概念的意思的了。
+# 2，“one” 是key的概念，"1 - 菜鸟教程" 和 "2 - 菜鸟工具" 是数值的概念
+tinydict = {'name': 'runoob','code':1, 'site': 'www.runoob.com'}
+print(tinydict.keys())
+print(tinydict.values())
+#下面是定义Dictionary的另外一些方式，可以看出这个python还是很多变的
+print(dict([('Runoob', 1), ('Google', 2), ('Taobao', 3)]))
+print({x: x**2 for x in (2, 4, 6)})
+print({x: x**3 for x in (2, 4, 6, 8)})
+print(dict(Runoob=1, Google=2, Taobao=3))
+# Python数据类型转换
+# https://www.runoob.com/python3/python3-data-type.html

b41860a87de8696919f5a09dbeef271912f88aed
Yan Zong <yan@pkcos.com>
2021-01-08 18:20:44 +0800

    Basic operators for python3

--- temp.py
+++ temp.py
@@ -162,3 +162,113 @@ print({x: x**3 for x in (2, 4, 6, 8)})
 print(dict(Runoob=1, Google=2, Taobao=3))
 # Python数据类型转换
 # https://www.runoob.com/python3/python3-data-type.html
+
+print("=================Basic operators==========================")
+a = 21
+b = 10
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b) # 除以
+print(a//b) # 除取整数 注意这两者的区别
+print(a%b) # 除取余数
+print(b**a)
+a=[1,2,3,4,5,6,7]
+if (n := len(a)) > 10: # := 海象运算符，可在表达式内部为变量赋值
+    print(f"List is too long ({n} elements, expected <= 10)")
+    # 上面这个print应该是可以打印除变量n的，但是呢，就是没有办法打印出来，这个就是
+    # 在以后的学习中需要注意一下
+
+a = 10
+b = 20
+if ( a and b ):
+   print ("1 - 变量 a 和 b 都为 true")
+else:
+   print ("1 - 变量 a 和 b 有一个不为 true")
+   
+if ( a or b ):
+   print ("2 - 变量 a 和 b 都为 true，或其中一个变量为 true")
+else:
+   print ("2 - 变量 a 和 b 都不为 true")   
+
+# 修改变量 a 的值
+a = 0
+if ( a and b ):
+   print ("3 - 变量 a 和 b 都为 true")
+else:
+   print ("3 - 变量 a 和 b 有一个不为 true")
+
+if ( a or b ):
+   print ("4 - 变量 a 和 b 都为 true，或其中一个变量为 true")
+else:
+   print ("4 - 变量 a 和 b 都不为 true")
+
+if not( a and b ):
+   print ("5 - 变量 a 和 b 都为 false，或其中一个变量为 false")
+else:
+   print ("5 - 变量 a 和 b 都为 true")
+   
+a = 10
+b = 20
+llist = [1, 2, 3, 4, 5 ]
+
+if (a in llist):
+    print("a is in list")
+else:
+    print("a is NOT in the list")
+
+if (b not in llist):
+    print("b is NOT in the list")
+else:
+    print("b is in the list")
+    
+a = 2
+
+if (a in llist):
+    print("a is in the list")
+else:
+    print("a is NOT in the list")
+    
+a= b = 20
+if (a is b):
+    print("a is the same with b")
+else:
+    print("a is different from b")
+
+# id()用来获取对象内存的地址    
+if (id(a) == id(b)):
+    print("a is the same with b")
+else:
+    print("the address of A is different from that of b")
+    
+b = 30
+
+if (a is b):
+    print("a is the same with b")
+else:
+    print("a is different from b")
+
+if (a is not b):
+    print("a is different from b")
+else:
+    print("a is teh same with b")
+    
+# is 用于判断两个变量引用对象是否为同一个， == 用于判断引用变量的值是否相等。
+# and 拥有更高优先级:
+x = True
+y = False
+z = False
+ 
+if x or y and z:
+    print("yes")
+else:
+    print("no")
+    
+    
+    
+    
+    
+    
+    
+    
+    

90e7059a3d4b4c5aa0d038f393c0f6ad1b993b20
Yan Zong <yan@yzong.com>
2021-01-22 07:43:02 +0800

    A visual intro to NumPy & data representation

---
+++ numpy_intro.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 07:40:32 2021
+
+@author: yan
+"""
+
+print('================Numpy============')
+print("https://jalammar.github.io/visual-numpy/")
+print('================Numpy============')  
+
+
+import numpy as np
